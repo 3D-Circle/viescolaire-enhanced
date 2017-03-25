5b81002808c96c7e40c6224b56e2149de196071f
trakjohnson <trakjohnsonweb@gmail.com>
2017-03-25 17:04:16 +0100

    another try on the fix for templates

--- hw_display/urls.py
+++ hw_display/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth.views import auth_login
 from . import views
 
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
+    url(r'^login/$', auth_login, {'template_name': 'hw_display/login.html'}),
 ]
---
+++ hw_display/user_handling.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.models import User
+import requests
+from bs4 import BeautifulSoup
+
+
+LOGIN_URL = "https: // viescolaire.ecolejeanninemanuel.net / auth.php"
+
+
+class CustomAuth:
+    """
+    Authenticate against the settings ADMIN_LOGIN and ADMIN_PASSWORD.
+
+    Use the login name and a hash of the password. For example:
+
+    ADMIN_LOGIN = 'admin'
+    ADMIN_PASSWORD = 'pbkdf2_sha256$30000$Vo0VlMnkR4Bk$qEvtdyZRWTcOsCnI/oQ7fVOu1XAURIZYoOZ3iq8Dr4M='
+    """
+
+    def authenticate(self, username=None, password=None):
+        playload = {'login': username, 'mdp': password}
+        user_session = requests.Session()
+        r = user_session.post(LOGIN_URL, data=playload)
+        response = BeautifulSoup(r.content, "html.parser")
+        login_success = "Erreur - Le mot de passe est incorrecte. Veuillez réessayer." not in response
+
+        if login_success:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                user = User(username=username)
+                user.is_staff = False
+                user.is_superuser = False
+                user.save()
+            return user
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
