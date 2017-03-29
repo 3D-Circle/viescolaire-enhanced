2a9a601002fd5b99b599e727ae105b609583e6fe
Trak Johnson <trakjohnsonweb@gmail.com>
2017-03-29 17:34:29 +0200

    rearranged url

--- hw_display/urls.py
+++ hw_display/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    url("^$", views.homepage, name="home"),
-    url("^render/$", views.vs_login, name="loginuser"),
+    url(r'^$', views.homepage, name="home"),
+    url(r'^render/$', views.vs_login, name="loginuser"),
     url(r'^login/$', views.login_render, name='loginform'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 ]
